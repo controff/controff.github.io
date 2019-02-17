1666bb01cea5046d2303cb87516dadc34075fa5e
controff <47724789+controff@users.noreply.github.com>
2019-02-18 00:29:50 +0300

    Upload project
    СКУД

---
+++ script.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+$(".link").on("click", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1250);
+});
